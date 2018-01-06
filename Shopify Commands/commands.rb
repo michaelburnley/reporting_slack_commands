b3594c9bc432735df988e82c68498de7d69c7e65
Michael Burnley <michael@cybercowgames.com>
2018-01-05 16:47:50 -0800

    Building out Customer Lookup

--- Shopify Commands/commands.rb
+++ Shopify Commands/commands.rb
@@ -16,10 +16,27 @@ ZOOSHOO = "https://#{ENV['ZOOSHOO_SHOPIFY_API_KEY']}:#{ENV['ZOOSHOO_SHOPIFY_SHAR
 JADAMS = "https://#{ENV['JADAMS_SHOPIFY_API_KEY']}:#{ENV['JADAMS_SHOPIFY_SHARED_SECRET']}@j-adams.myshopify.com/admin"
 ###### Basic Config Information #####
 
+###### Hash for easier Timeframe calculation ######
+timeframes = {
+	"today" => Time.now.iso8601,
+	"yesterday" => (Time.now - (60 * 60 * 24)).iso8601,
+	"seven_days" => (Time.now - (60 * 60 * 24 * 7)).iso8601,
+	"fifteen_days" => (Time.now - (60 * 60 * 24  * 15)).iso8601,
+	"thirty_days" => (Time.now - (60 * 60 * 24  * 30)).iso8601,
+	"sixty_days" => (Time.now - (60 * 60 * 24  * 60)).iso8601,
+	"ninety_days" => (Time.now - (60 * 60 * 24  * 90)).iso8601,
+	"six_months" => (Time.now - (60 * 60 * 24  * 180)).iso8601,
+	"year" => (Time.now - (60 * 60 * 24  * 365)).iso8601
+}
+###### Hash for easier Timeframe calculation ######
+
+###### RestClient Wrapper ######
 def request(url_base, request, url, params = nil)
 	RestClient.send(request.to_sym, "#{url_base}#{url}.json", { params: params.to_h})
 end
+###### RestClient Wrapper ######
 
+###### Grab Refund information from Shopify ######
 def refund_info(dates, site)
 	params, refunded_orders_ids, refunded_orders_names = [],[],[]
 	refunded_orders_amount = 0
@@ -43,7 +60,9 @@ def refund_info(dates, site)
 		"refund_amounts" => refunded_orders_amount
 	}
  end
+###### Grab Refund information from Shopify ######
 
+###### Grab Top Selling SKUs and Amount Sold for a Timeframe ######
 def sales_info(dates, site)
 	params, orders_ids, orders_names, all_skus, orders_request, orders = [],[],[],[],[],[]
 	orders_amount = 0
@@ -88,42 +107,56 @@ def sales_info(dates, site)
 		"order_ids" => orders_ids,
 		"order_numbers" => orders_names,
 		"refund_amounts" => orders_amount,
-		"skus" => all_skus,
+		"skus" => all_skus
 	}
- end
+end
+###### Grab Top Selling SKUs and Amount Sold for a Timeframe ######
 
+###### Lookup an Order by Order Number ######
 def order_lookup(order_name, site)
-	params, order_info = [], []
+	if order_name.include? "ZJI"
+		site = ZOOJI
+	end
+	params, order_info, skus = [], [], []
 	params.push(['name',order_name],['status', 'any'])
 	order = JSON.parse(request(site, 'get', "/orders", params).body)
 	order_hash = order['orders'][0]
 
+	order_hash['line_items'].each {|line| skus.push(line['sku'])}
+
 	order_info = {
 		"email" => order_hash['email'],
 		"total_price" => order_hash['total_price'],
-		"fulfillment_status" => order_hash['fulfillment_status']
+		"fulfillment_status" => order_hash['fulfillment_status'],
+		"skus" => skus
 	}
 end
+###### Lookup an Order by Order Number ######
 
+###### Lookup a Customer ######
 def customer_lookup(email, site)
-	params = []
-	params.push(['email', email],['limit', 1])
-	customer = JSON.parse(request(site, 'get', '/customers', params).body)
+	params, order_arr = [], []
+	amount_spent = 0
+	params.push(['query', email],['limit', 10])
+	customer = JSON.parse(request(site, 'get', '/customers/search', params).body)
 	customer_id = customer['customers'][0]['id']
-	customer_orders = JSON.parse(request(site, 'get', "/customers/#{customer_id}/orders"))
+	params.clear.push(['status', 'any'])
+	customer_orders = JSON.parse(request(site, 'get', "/customers/#{customer_id}/orders", params).body)
+
+	customer_orders['orders'].each do |order|
+		amount_spent = amount_spent + order['total_price'].to_f
+		order_arr.push(order['name'])
+	end
+
+	customer_info = {
+		'order_count' => customer['customers'][0]['orders_count'],
+		'all_orders' => order_arr,
+		'most_recent_order' => order_arr[0],
+		'amount_spent' => amount_spent.round(2)
+	}
+
 end
+###### Lookup a Customer ######
 
-timeframes = {
-	"today" => Time.now.iso8601,
-	"yesterday" => (Time.now - (60 * 60 * 24)).iso8601,
-	"seven_days" => (Time.now - (60 * 60 * 24 * 7)).iso8601,
-	"fifteen_days" => (Time.now - (60 * 60 * 24  * 15)).iso8601,
-	"thirty_days" => (Time.now - (60 * 60 * 24  * 30)).iso8601,
-	"sixty_days" => (Time.now - (60 * 60 * 24  * 60)).iso8601,
-	"ninety_days" => (Time.now - (60 * 60 * 24  * 90)).iso8601,
-	"six_months" => (Time.now - (60 * 60 * 24  * 180)).iso8601,
-	"year" => (Time.now - (60 * 60 * 24  * 365)).iso8601
-}
 
-#puts order_lookup("#105141", ZOOSHOO)
-#customer_lookup("leetialyonsneo@hotmail.com", ZOOSHOO)
+#puts order_lookup("#105154", ZOOSHOO)
